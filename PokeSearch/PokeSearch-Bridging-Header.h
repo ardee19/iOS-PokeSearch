f8cded806ad91537957dd9dd21a8c4362030cf25
Roy Dimayuga <mr_nicelittleboy@yahoo.com>
2016-11-03 17:01:54 -0700

    Initial Commit with Pod Setup

---
+++ PokeSearch/PokeSearch-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeSearch-Bridging-Header.h
+//  PokeSearch
+//
+//  Created by Roy Dimayuga on 11/3/16.
+//  Copyright © 2016 http://www.WeAreSuperAwesome.net. All rights reserved.
+//
+
+#import "GeoFire.h"
---
+++ PokeSearch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PokeSearch
+//
+//  Created by Roy Dimayuga on 11/3/16.
+//  Copyright © 2016 http://www.WeAreSuperAwesome.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var geoFire: GeoFire!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
